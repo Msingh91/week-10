c62942279bb54165a459af974040f8b00f29f63b
Msingh91 <manishsingh@Manishs-MacBook-Pro.local>
2017-05-27 17:13:55 -0500

    ok

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: "6OzzxwDbOQfkoHbYjT2jEcSQ8",
+  consumer_secret: "QqtoOnJ1Q1Lp2S3fZeXISSQak3NSc7vSzzH0DR4KmRWzHmknHs",
+  access_token_key: "868163518113226752-9nILbnVEIhdq2trbGamLt47nnsjIYcm",
+  access_token_secret: "5o63zNjKi2k0Pc1Gzlcrn6jK1o3SVi77bd0uPBh2bohOS",
+}
